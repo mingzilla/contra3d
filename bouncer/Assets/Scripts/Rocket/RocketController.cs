22ea1b7d7bdd9ed438b717a14ac610279194803b
ming.huang <means789@gmail.com>
2022-04-18 14:15:52 +0100

    1

---
+++ bouncer/Assets/Scripts/Rocket/RocketController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BaseUtil.GameUtil;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Rocket
+{
+    public class RocketController : MonoBehaviour
+    {
+        private UserInput userInput = UserInput.Create(0);
+        private Rigidbody rb;
+
+        public float rotateSpeed = 200f;
+        public float boostSpeed = 1000f;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            float deltaTime = Time.deltaTime;
+            if (userInput.horizontal != 0)
+            {
+                float speed = 0 - (rotateSpeed * userInput.horizontal * deltaTime);
+                transform.Rotate(Vector3.forward * speed);
+            }
+            if (userInput.fire1)
+            {
+                rb.AddRelativeForce(Vector3.up * boostSpeed * deltaTime);
+            }
+
+            UserInput.ResetTriggers(userInput);
+        }
+
+        public void Move(InputAction.CallbackContext context)
+        {
+            userInput = UserInput.Move(userInput, context);
+        }
+
+        public void Boost(InputAction.CallbackContext context)
+        {
+            if (context.started) userInput.fire1 = true;
+        }
+    }
+}
---
+++ bouncer/Assets/Scripts/Rocket/RocketDestinationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Types;
+using UnityEngine;
+
+namespace Rocket
+{
+    public class RocketDestinationController : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag(GameTag.PLAYER.name))
+            {
+                Debug.Log("Yay!");
+            }
+        }
+    }
+}
