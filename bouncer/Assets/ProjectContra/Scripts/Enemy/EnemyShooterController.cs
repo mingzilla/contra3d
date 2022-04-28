a3167b866f0397727d1d6378fb722719bbc8db78
ming.huang <means789@gmail.com>
2022-04-29 00:43:09 +0100

    1

---
+++ bouncer/Assets/ProjectContra/Scripts/Enemy/EnemyShooterController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using BaseUtil.GameUtil.Base;
+using ProjectContra.Scripts.AppSingleton.LiveResource;
+using ProjectContra.Scripts.EnemyBullet;
+using ProjectContra.Scripts.GameData;
+using ProjectContra.Scripts.Player;
+using ProjectContra.Scripts.Types;
+using UnityEngine;
+
+namespace ProjectContra.Scripts.Enemy
+{
+    public class EnemyShooterController : EnemyController
+    {
+        public int damage = 1;
+
+        private GameStoreData storeData;
+        private Rigidbody rb;
+        private CapsuleCollider theCollider;
+        private LayerMask destructibleLayers;
+        private GameObject destroyEffect;
+
+        private bool canFireShot = true;
+
+        void Start()
+        {
+            storeData = AppResource.instance.storeData;
+            gameObject.layer = GameLayer.ENEMY.GetLayer();
+            rb = UnityFn.AddRigidBodyAndFreezeZ(gameObject);
+            destructibleLayers = GameLayer.GetLayerMask(new List<GameLayer>() {GameLayer.PLAYER});
+            destroyEffect = AppResource.instance.smallExplosionPrefab;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            FireShots();
+        }
+
+        void FireShots()
+        {
+            if (!storeData.HasPlayer()) return;
+            if (canFireShot)
+            {
+                canFireShot = false;
+                UnityFn.SetTimeout(this, 3, () =>
+                {
+                    canFireShot = true;
+                    Vector3 position = transform.position;
+                    EnemyBasicBulletController.Spawn(position, storeData.GetClosestPlayer(position).inGameTransform, EnemyBulletType.BASIC);
+                });
+            }
+        }
+
+        void OnCollisionEnter(Collision other)
+        {
+            DealDamage(other);
+        }
+
+        void OnCollisionStay(Collision other)
+        {
+            DealDamage(other);
+        }
+
+        void DealDamage(Collision other)
+        {
+            if (GameLayer.Matches(other.gameObject.layer, GameLayer.PLAYER))
+            {
+                Vector3 location = transform.position;
+                GameFn.DealDamage(location, 2, destructibleLayers, (obj) =>
+                {
+                    CharacterInGameController character = obj.GetComponent<CharacterInGameController>();
+                    character.TakeDamage(location, damage);
+                });
+            }
+        }
+
+        public override void TakeDamage(Vector3 position, int damage)
+        {
+            UnityFn.CreateEffect(destroyEffect, position, 1f);
+            Destroy(gameObject);
+        }
+    }
+}
