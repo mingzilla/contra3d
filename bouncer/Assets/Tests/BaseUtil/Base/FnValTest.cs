59cf838328b6253d04a8907ad74d05b208bad466
ming.huang <means789@gmail.com>
2022-05-08 11:35:52 +0100

    1

--- bouncer/Assets/Tests/BaseUtil/Base/FnValTest.cs
+++ bouncer/Assets/Tests/BaseUtil/Base/FnValTest.cs
@@ -24,5 +24,21 @@ namespace Tests.BaseUtil.Base
             Assert.AreEqual(FnVal.StartsWith(null)("null"), false);
             Assert.AreEqual(FnVal.StartsWith(null)(""), false);
         }
+
+        [Test]
+        public void GetNextIndex()
+        {
+            Assert.AreEqual(FnVal.GetNextIndex(2, 3), 0);
+            Assert.AreEqual(FnVal.GetNextIndex(1, 3), 2);
+            Assert.AreEqual(FnVal.GetNextIndex(0, 3), 1);
+        }
+
+        [Test]
+        public void GetPreviousIndex()
+        {
+            Assert.AreEqual(FnVal.GetPreviousIndex(2, 3), 1);
+            Assert.AreEqual(FnVal.GetPreviousIndex(1, 3), 0);
+            Assert.AreEqual(FnVal.GetPreviousIndex(0, 3), 2);
+        }
     }
 }
