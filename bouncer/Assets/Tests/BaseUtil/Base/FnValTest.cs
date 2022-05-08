7cafa32b85d7f91a50e3287dec85bff68cd81c4d
ming.huang <means789@gmail.com>
2022-05-08 11:40:25 +0100

    1

--- bouncer/Assets/Tests/BaseUtil/Base/FnValTest.cs
+++ bouncer/Assets/Tests/BaseUtil/Base/FnValTest.cs
@@ -26,6 +26,48 @@ namespace Tests.BaseUtil.Base
         }
 
         [Test]
+        public void AtMostF()
+        {
+            Assert.AreEqual(FnVal.AtMostF(1, 0), 0);
+            Assert.AreEqual(FnVal.AtMostF(1, 1), 1);
+            Assert.AreEqual(FnVal.AtMostF(1, 2), 1);
+        }
+
+        [Test]
+        public void AtMost()
+        {
+            Assert.AreEqual(FnVal.AtMost(1, 0), 0);
+            Assert.AreEqual(FnVal.AtMost(1, 1), 1);
+            Assert.AreEqual(FnVal.AtMost(1, 2), 1);
+        }
+
+        [Test]
+        public void AtLeastF()
+        {
+            Assert.AreEqual(FnVal.AtLeastF(1, 0), 1);
+            Assert.AreEqual(FnVal.AtLeastF(1, 1), 1);
+            Assert.AreEqual(FnVal.AtLeastF(1, 2), 2);
+        }
+
+        [Test]
+        public void AtLeast()
+        {
+            Assert.AreEqual(FnVal.AtLeast(1, 0), 1);
+            Assert.AreEqual(FnVal.AtLeast(1, 1), 1);
+            Assert.AreEqual(FnVal.AtLeast(1, 2), 2);
+        }
+
+        [Test]
+        public void Clamp()
+        {
+            Assert.AreEqual(FnVal.Clamp(2, 0, 2), 2);
+            Assert.AreEqual(FnVal.Clamp(3, 0, 2), 2);
+            Assert.AreEqual(FnVal.Clamp(1, 0, 2), 1);
+            Assert.AreEqual(FnVal.Clamp(0, 0, 2), 0);
+            Assert.AreEqual(FnVal.Clamp(-1, 0, 2), 0);
+        }
+
+        [Test]
         public void GetNextIndex()
         {
             Assert.AreEqual(FnVal.GetNextIndex(2, 3), 0);
