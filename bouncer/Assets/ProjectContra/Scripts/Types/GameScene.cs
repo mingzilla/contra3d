221b3a06d7f70622f6480e3dfe22fdad1b9608a9
ming.huang <means789@gmail.com>
2022-05-14 01:53:32 +0100

    1

--- bouncer/Assets/ProjectContra/Scripts/Types/GameScene.cs
+++ bouncer/Assets/ProjectContra/Scripts/Types/GameScene.cs
@@ -12,6 +12,9 @@ namespace ProjectContra.Scripts.Types
         public static readonly GameScene TITLE_SCREEN = Create("ContraTitle", false, GameControlState.TITLE_SCREEN_MENU, "");
         public static readonly GameScene LEVEL_1 = Create("ContraLv1", true, GameControlState.INFO_SCREEN, "Area 1");
         public static readonly GameScene LEVEL_2 = Create("ContraLv2", true, GameControlState.INFO_SCREEN, "Area 2");
+        public static readonly GameScene LEVEL_3 = Create("ContraLv3", true, GameControlState.INFO_SCREEN, "Area 3");
+        public static readonly GameScene LEVEL_4 = Create("ContraLv4", true, GameControlState.INFO_SCREEN, "Area 4");
+        public static readonly GameScene LEVEL_5 = Create("ContraLv5", true, GameControlState.INFO_SCREEN, "Area 5");
 
         public string name;
         public int index;
@@ -28,6 +31,9 @@ namespace ProjectContra.Scripts.Types
                 TITLE_SCREEN,
                 LEVEL_1,
                 LEVEL_2,
+                LEVEL_3,
+                LEVEL_4,
+                LEVEL_5,
             };
         }
 
