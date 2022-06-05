6eafecc8d71c3348ec0b9b5dc8b6cfeea0bbc3e1
ming.huang <means789@gmail.com>
2022-06-05 01:11:10 +0100

    partical 3

---
+++ contra_3d/Assets/ProjectContra/_ImportedAssets/ParticalTests/Project3/RepeatFire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatFire : MonoBehaviour {
+
+    public GameObject fireObject;
+    public float timeInterval;
+    private float timeIncrement;
+    public bool isFiring = true;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFiring)
+        {
+            timeIncrement += Time.deltaTime;
+            if(timeIncrement >= timeInterval)
+            {
+                Instantiate(fireObject, transform.position, transform.rotation);
+                timeIncrement = 0f;
+            }
+        }
+	}
+}
