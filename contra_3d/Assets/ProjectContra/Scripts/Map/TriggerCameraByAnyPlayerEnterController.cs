ce4515e7ca442e4400872f8c7491d93e1fd9774d
ming.huang <means789@gmail.com>
2022-06-29 13:13:27 +0100

    improved lv4 camera

---
+++ contra_3d/Assets/ProjectContra/Scripts/Map/TriggerCameraByAnyPlayerEnterController.cs
@@ -0,0 +1,29 @@
+﻿using ProjectContra.Scripts.Types;
+using UnityEngine;
+
+namespace ProjectContra.Scripts.Map
+{
+    public class TriggerCameraByAnyPlayerEnterController : MonoBehaviour
+    {
+        public bool isActivated = false; // used to be checked to trigger action
+
+        [SerializeField] private GameObject oldCamera;
+        [SerializeField] private GameObject newCamera;
+
+        void Start()
+        {
+            gameObject.layer = GameLayer.INVISIBLE_WALL_TO_PLAYER.GetLayer();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (isActivated) return;
+            if (GameLayer.Matches(other.gameObject.layer, GameLayer.PLAYER))
+            {
+                isActivated = true;
+                newCamera.SetActive(true);
+                oldCamera.SetActive(false);
+            }
+        }
+    }
+}
