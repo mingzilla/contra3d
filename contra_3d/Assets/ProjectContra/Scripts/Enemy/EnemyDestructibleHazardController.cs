0b4c57112eb4ad2d882b9ddd4a8cce3c13bd1c1c
ming.huang <means789@gmail.com>
2022-07-09 13:55:52 +0100

    added rolling balls

---
+++ contra_3d/Assets/ProjectContra/Scripts/Enemy/EnemyDestructibleHazardController.cs
@@ -0,0 +1,71 @@
+﻿using BaseUtil.GameUtil.Base;
+using ProjectContra.Scripts.AbstractController;
+using ProjectContra.Scripts.AppSingleton.LiveResource;
+using ProjectContra.Scripts.Enemy.Util;
+using ProjectContra.Scripts.GameData;
+using ProjectContra.Scripts.Types;
+using UnityEngine;
+
+namespace ProjectContra.Scripts.Enemy
+{
+    public class EnemyDestructibleHazardController : AbstractDestructibleController
+    {
+        private GameStoreData storeData; // Hazard
+
+        public int hp = 5;
+        [SerializeField] private float detectionRange = 40f;
+        [SerializeField] private int damage = 1;
+        private Rigidbody rb;
+        private bool isActive = false;
+
+        private void Start()
+        {
+            storeData = AppResource.instance.storeData;
+            gameObject.layer = GameLayer.ENEMY.GetLayer();
+            rb = gameObject.GetComponent<Rigidbody>();
+            rb.useGravity = isActive;
+        }
+
+        void Update()
+        {
+            if (!isActive)
+                RunIfPlayerIsInRange(storeData, GetDetectionRange(), (closestPlayer) =>
+                {
+                    isActive = true;
+                    rb.useGravity = isActive;
+                });
+        }
+
+        void OnCollisionEnter(Collision other)
+        {
+            DealDamage(other);
+        }
+
+        void OnCollisionStay(Collision other)
+        {
+            DealDamage(other);
+        }
+
+        void DealDamage(Collision other)
+        {
+            EnemyUtil.DealDamageToPlayer(transform.position, damage, other.collider);
+        }
+
+        public override void TakeDamage(Vector3 position, int damage)
+        {
+            AppSfx.Play(AppSfx.instance.bigEnemyDamaged);
+            hp -= damage;
+            if (hp <= 0)
+            {
+                UnityFn.CreateEffect(AppResource.instance.enemyDestroyedSmallExplosion, position, 1f);
+                AppSfx.Play(AppSfx.instance.grenadeExploded);
+                Destroy(gameObject);
+            }
+        }
+
+        public override float GetDetectionRange()
+        {
+            return detectionRange;
+        }
+    }
+}
