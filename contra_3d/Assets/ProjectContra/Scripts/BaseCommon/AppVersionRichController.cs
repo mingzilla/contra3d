4d5f84ff1a2e8c9acf92498277712ba75be83f80
ming.huang <means789@gmail.com>
2022-08-06 12:45:35 +0100

    implemented auto version number

---
+++ contra_3d/Assets/ProjectContra/Scripts/BaseCommon/AppVersionRichController.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ProjectContra.Scripts.BaseCommon
+{
+    public class AppVersionRichController : MonoBehaviour
+    {
+        private void Start()
+        {
+            TMP_Text text = gameObject.GetComponent<TMP_Text>(); // The TextMeshPro component is TMP_Text
+            if (text != null) text.text = $"v {Application.version}";
+        }
+    }
+}
