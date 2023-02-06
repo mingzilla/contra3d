46963b8a9b82e3a71bd6f6848ae0e98ba22d264a
ming.huang <means789@gmail.com>
2023-02-06 01:13:48 +0000

    1

---
+++ ui_builder_components/UI Builder Components/Assets/ProjectContent/Scripts/Types/SelectionStateForSkillPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using BaseUtil.GameUtil.Types;
+
+namespace ProjectContent.Scripts.Types
+{
+    public class SelectionStateForSkillPanel : UiItemSelectionState
+    {
+        public static readonly UiItemSelectionState IS_SET_1_A_ON = Create("IS_SET_1_A_ON");
+        public static readonly UiItemSelectionState IS_SET_1_B_ON = Create("IS_SET_1_B_ON");
+        public static readonly UiItemSelectionState IS_SET_1_X_ON = Create("IS_SET_1_X_ON");
+        public static readonly UiItemSelectionState IS_SET_1_Y_ON = Create("IS_SET_1_Y_ON");
+
+        public static readonly UiItemSelectionState IS_SET_2_A_ON = Create("IS_SET_2_A_ON");
+        public static readonly UiItemSelectionState IS_SET_2_B_ON = Create("IS_SET_2_B_ON");
+        public static readonly UiItemSelectionState IS_SET_2_X_ON = Create("IS_SET_2_X_ON");
+        public static readonly UiItemSelectionState IS_SET_2_Y_ON = Create("IS_SET_2_Y_ON");
+        
+        static List<UiItemSelectionState> All()
+        {
+            return new List<UiItemSelectionState>()
+            {
+                IS_VISIBLE,
+                IS_INVISIBLE,
+                IS_ENABLED,
+                IS_DISABLED,
+                IS_SELECTED,
+                IS_SET_1_A_ON,
+                IS_SET_1_B_ON,
+                IS_SET_1_X_ON,
+                IS_SET_1_Y_ON,
+                IS_SET_2_A_ON,
+                IS_SET_2_B_ON,
+                IS_SET_2_X_ON,
+                IS_SET_2_Y_ON,
+            };
+        }
+    }
+}
---
+++ ui_builder_components/UI Builder Components/Assets/Scripts/BaseUtil/GameUtil/Types/UiItemSelectionState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using BaseUtil.Base;
+
+namespace BaseUtil.GameUtil.Types
+{
+    public class UiItemSelectionState
+    {
+        public static readonly UiItemSelectionState IS_VISIBLE = Create("IS_VISIBLE"); // normal state - present but not on
+        public static readonly UiItemSelectionState IS_INVISIBLE = Create("IS_INVISIBLE"); // not visible but reserves space
+        public static readonly UiItemSelectionState IS_ENABLED = Create("IS_ENABLED"); // green - toggled on
+        public static readonly UiItemSelectionState IS_DISABLED = Create("IS_DISABLED"); // grey - disabled, not possible to be enabled
+        public static readonly UiItemSelectionState IS_SELECTED = Create("IS_SELECTED"); // red box - cursor is on the item
+
+        public string name;
+
+        static Dictionary<string, UiItemSelectionState> typeMap = Fn.ListToDictionaryWithKeyFn((x) => x.name, All());
+
+        static List<UiItemSelectionState> All()
+        {
+            return new List<UiItemSelectionState>()
+            {
+                IS_VISIBLE,
+                IS_INVISIBLE,
+                IS_ENABLED,
+                IS_DISABLED,
+                IS_SELECTED,
+            };
+        }
+
+        protected static UiItemSelectionState Create(string name)
+        {
+            var layer = new UiItemSelectionState
+            {
+                name = name
+            };
+            return layer;
+        }
+
+        public static UiItemSelectionState GetByName(string name)
+        {
+            return typeMap[(name)];
+        }
+    }
+}
